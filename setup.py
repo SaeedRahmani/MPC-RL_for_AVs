727a03e2ca1b480435a8470cd8a9c39017c74a3e
Zhenlin-Xu <Z.Xu-28@student.tudelft.nl>
2024-11-09 09:14:49 +0100

    Fix the bug in the setup.py file.

--- setup.py
+++ setup.py
@@ -14,8 +14,8 @@ setup(
         'highway-env==1.9.1',
         'casadi==3.6.6',
         'hydra-core==1.3.2',
-        'stable-baselines3==2.3.1'
-        'shapely==2.0.6'
+        'stable-baselines3==2.3.1',
+        'shapely==2.0.6',
     ],
     classifiers=[
         'Programming Language :: Python :: 3',  
